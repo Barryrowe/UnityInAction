6fe53bc1f9f90b82ad7b59d12d834e51d2eac258
Barryrowe <barryrowe@gmail.com>
2015-10-18 12:12:33 -0400

    Finish Chapter2 Example
    Working movement scripts and 3D environment.

---
+++ Chapter2/Assets/MouseLook.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseLook : MonoBehaviour {
+
+	public enum RotationAxes {
+		MouseXAndY = 0,
+		MouseX = 1, 
+		MouseY = 2
+	}
+
+	public RotationAxes axes = RotationAxes.MouseXAndY;
+
+	public float sensitivityHorizontal = 9.0f;
+	public float sensitivityVertical = 9.0f;
+
+	public float minimumVert = -45.0f;
+	public float maximumVert = 45.0f;
+
+	private float _rotationX = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+		Rigidbody rigidBody = GetComponent<Rigidbody> ();
+		if (rigidBody != null) {
+			rigidBody.freezeRotation = true;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (axes == RotationAxes.MouseX) {
+			//horizontal rotation here
+			float rotation = sensitivityHorizontal * Input.GetAxis("Mouse X");
+			transform.Rotate (0, rotation, 0);
+		} else if (axes == RotationAxes.MouseY) {
+			//vertical rotation here
+			_rotationX -= Input.GetAxis("Mouse Y") * sensitivityVertical;
+			_rotationX = Mathf.Clamp (_rotationX, minimumVert, maximumVert);
+
+			float rotationY = transform.localEulerAngles.y;
+			transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+		} else {
+			//Both rotation here
+			_rotationX -= Input.GetAxis("Mouse Y") * sensitivityVertical;
+			_rotationX = Mathf.Clamp (_rotationX, minimumVert, maximumVert);
+
+			float delta = Input.GetAxis("Mouse X") * sensitivityHorizontal;
+			float rotationY = transform.localEulerAngles.y + delta;
+
+			transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+		}
+	}
+}
---
+++ Chapter2/Assets/Spin.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spin : MonoBehaviour {
+
+	public float speed = 3.0f;
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.Rotate (0, speed * Time.fixedDeltaTime, 0);
+	}
+}
